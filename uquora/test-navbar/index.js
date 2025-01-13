6ddad4e202e667433ffc9887a12862cf1d3d6532
¨camila-stefano¨ <camilavictoriastefano@gmail.com>
2025-01-13 12:29:34 -0300

    test uqora

--- uquora/test-navbar/index.js
+++ uquora/test-navbar/index.js
@@ -4,3 +4,25 @@ fontAwesomeCDN.rel = "stylesheet";
 fontAwesomeCDN.href =
   "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css";
 document.head.appendChild(fontAwesomeCDN);
+
+const productsDropdown = document.querySelector(".products-dropdown");
+
+const floatingMenuContainer = document.querySelector(
+  ".floating-menu-container"
+);
+
+productsDropdown.addEventListener("mouseover", () => {
+  floatingMenuContainer.classList.add("display-block");
+});
+
+productsDropdown.addEventListener("mouseout", () => {
+  floatingMenuContainer.classList.remove("display-block");
+});
+
+floatingMenuContainer.addEventListener("mouseover", () => {
+  floatingMenuContainer.classList.add("display-block");
+});
+
+floatingMenuContainer.addEventListener("mouseout", () => {
+  floatingMenuContainer.classList.remove("display-block");
+});
