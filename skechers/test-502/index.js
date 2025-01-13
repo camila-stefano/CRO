ee17bdc40b1070e884a7d50ebdbdcb9ddc1dc214
¨camila-stefano¨ <camilavictoriastefano@gmail.com>
2025-01-13 12:29:02 -0300

    skechers test 502

---
+++ skechers/test-502/index.js
@@ -0,0 +1,71 @@
+const cartContainer = document.querySelector(".cart-and-ipay");
+const addToCartBtn = cartContainer.firstElementChild;
+const wishlistBtn = document.querySelector(".c-product-details__wishlist-btn");
+const wishlistSeparator = document.querySelector(
+  ".c-product-details__ratings-and-wishlist__separator"
+);
+const reviews = document.querySelector(
+  ".c-product-details__ratings-and-wishlist"
+);
+
+function handleSmallDevices() {
+  const isMobile = window.innerWidth < 544;
+  const isTabletMobile = window.innerWidth < 768;
+  const isTablet = window.innerWidth < 1279;
+  const isDesktop = window.innerWidth >= 1279;
+
+  const heartBtn = document.querySelector(
+    ".c-product-details__wishlist-btn__heart"
+  );
+  const element = document.querySelector(
+    ".c-product-details__wishlist-btn__text.pl-1"
+  );
+
+  if (isDesktop) {
+    reviews.insertAdjacentElement("beforeend", wishlistBtn);
+    wishlistSeparator.style.display = "block";
+    wishlistBtn.style.margin = "0";
+    wishlistBtn.style.paddingTop = "0";
+  }
+
+  if (isTablet) {
+    cartContainer.insertAdjacentElement("beforeend", wishlistBtn);
+    cartContainer.style.flexDirection = "column";
+    addToCartBtn.classList.add("col-10");
+    addToCartBtn.classList.remove("col-md");
+    wishlistSeparator.style.display = "none";
+    wishlistBtn.style.justifyContent = "center";
+    wishlistBtn.style.margin = "auto";
+    wishlistBtn.style.paddingTop = "1rem";
+    wishlistBtn.style.width = "fit-content";
+  }
+
+  if (isTabletMobile) {
+    wishlistBtn.style.justifyContent = "left";
+    wishlistBtn.style.margin = "0";
+    wishlistBtn.style.paddingTop = "0";
+  }
+
+  if (isMobile) {
+    cartContainer.className = "cart-and-ipay align-items-center flex-row";
+
+    heartBtn.style.width = "2.5rem";
+    heartBtn.style.height = "2.5rem";
+    heartBtn.classList.add("m-0");
+    wishlistBtn.style.margin = "0";
+    wishlistBtn.style.paddingTop = "0";
+
+    element.style.display = "none";
+  } else {
+    cartContainer.className = "row cart-and-ipay";
+
+    heartBtn.style.width = "2rem";
+    heartBtn.style.height = "2rem";
+    heartBtn.classList.remove("m-0");
+
+    element.style.display = "";
+  }
+}
+
+(() => handleSmallDevices())();
+window.addEventListener("resize", () => handleSmallDevices());
