259cfd979ca8ebe57e5ceb3705ffcc138e0a3f3b
¨camila-stefano¨ <camilavictoriastefano@gmail.com>
2025-04-11 16:54:59 -0300

    turboTax test 1

---
+++ TurboTax/test-1/index.js
@@ -0,0 +1,33 @@
+const variantContainer = document.querySelector(".test-1-container");
+
+const newVariantElement = document.createElement("div");
+newVariantElement.classList.add("test-1-container");
+newVariantElement.innerHTML = `
+  <div class="test-1-container-card">
+      <div class="test-1-container-card-text">
+        <h3 class="title">File Your Own Taxes</h3>
+        <h3 class="subtitle">
+          Just answer simple questions, and we’ll guide you through filing your
+          taxes.
+        </h3>
+      </div>
+      <div class="test-1-container-card-button">
+        <a
+          type="button"
+          href="https://myturbotax.intuit.com/sign-up?cid=soc_blog_button_file"
+          target="_self"
+          class=""
+          ><span>Get started</span></a
+        >
+      </div>
+      <div class="test-1-container-card-link">
+        <a
+          class=""
+          href="https://turbotax.intuit.com/personal-taxes/online/file-your-own-taxes/"
+          ><span class="">See how TurboTax works</span></a
+        >
+      </div>
+    </div>
+`;
+
+variantContainer.appendChild(newVariantElement);
