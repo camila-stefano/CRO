e8cb2fcdcf539f186ae70bb454f996dcd9f88433
¨camila-stefano¨ <camilavictoriastefano@gmail.com>
2025-05-07 16:10:59 -0300

    code updated with observer

--- Titleist/test-48-Test XX: PDP Image Grid Test/index.js
+++ Titleist/test-48-Test XX: PDP Image Grid Test/index.js
@@ -24,3 +24,28 @@ $(document).ready(function () {
 $(window).on("resize", function () {
   toggleSlickSlider();
 });
+
+//obsrva el cambio por el scroll para evitar que se cree el elemento height
+const observer = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    mutation.addedNodes.forEach((node) => {
+      if (
+        node.nodeType === 1 && // Asegura que sea un elemento HTML
+        node.classList.contains("product-image-container-height")
+      ) {
+        node.remove();
+      }
+      // Si el nuevo nodo tiene hijos donde podría estar el target
+      if (node.querySelectorAll) {
+        const nested = node.querySelectorAll(".product-image-container-height");
+        nested.forEach((el) => el.remove());
+      }
+    });
+  });
+});
+
+// Empieza a observar el body en busca de nuevos elementos añadidos al DOM
+observer.observe(document.body, {
+  childList: true,
+  subtree: true,
+});
